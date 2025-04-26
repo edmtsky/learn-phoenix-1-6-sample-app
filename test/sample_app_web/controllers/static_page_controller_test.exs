f787deae7615ac0c9f3c232799106d5c8b1b3845
edmtsky <161596840+edmtsky@users.noreply.github.com>
2025-04-26 18:52:12 +0300

    move away from layout, add raw html code for home,help,about pages with titles

--- test/sample_app_web/controllers/static_page_controller_test.exs
+++ test/sample_app_web/controllers/static_page_controller_test.exs
@@ -2,22 +2,27 @@
 defmodule SampleAppWeb.StaticPageControllerTest do
   use SampleAppWeb.ConnCase
 
+  @base_title "Phoenix Tutorial Sample App"
+
   test "should get home", %{conn: conn} do
     conn = get(conn, Routes.static_page_path(conn, :home))
 
-    assert html_response(conn, 200)
+    html_response(conn, 200)
+    |> assert_select("title", "Home | #{@base_title}")
   end
 
   test "should get help", %{conn: conn} do
     conn = get(conn, Routes.static_page_path(conn, :help))
 
-    assert html_response(conn, 200)
+    html_response(conn, 200)
+    |> assert_select("title", "Help | #{@base_title}")
   end
 
   test "should get about", %{conn: conn} do
     conn = get(conn, Routes.static_page_path(conn, :about))
 
-    assert html_response(conn, 200)
+    html_response(conn, 200)
+    |> assert_select("title", "About | #{@base_title}")
   end
 
 end
